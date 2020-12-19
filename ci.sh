b54e6f05fc588775a381307e4108ce055f4a12a9
Michał Budziak <michal.mariusz.b@gmail.com>
2020-12-19 17:13:55 +0100

    Compile plugins

---
+++ ci.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: ./ci.sh <version> <mode>"
+    exit 1
+fi
+
+BUILD_VERSION=$1
+MODE=${2:-watch}
+DIR="$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}" ) )" && pwd )"
+
+# PREPARE
+mkdir -p "$DIR/plugins"
+chmod 777 "$DIR/plugins"
+rm -f $DIR/plugins/*
+
+docker run \
+    --rm \
+    -it \
+    -v "$DIR/plugins":/home/builder/builds \
+    -v "$DIR/scripting":/home/builder/sources \
+    -v "$DIR/scripting/include/colorchat.inc":/home/builder/addons/amxmodx/scripting/include/colorchat.inc \
+    -v "$DIR/scripting/include/custom_color_chat.inc":/home/builder/addons/amxmodx/scripting/include/custom_color_chat.inc \
+    -v "$DIR/scripting/include/shopsms_const.inc":/home/builder/addons/amxmodx/scripting/include/shopsms_const.inc \
+    -v "$DIR/scripting/include/shopsms.inc":/home/builder/addons/amxmodx/scripting/include/shopsms.inc \
+    "budziam/amxx-builder:${BUILD_VERSION}" \
+    ${MODE}
